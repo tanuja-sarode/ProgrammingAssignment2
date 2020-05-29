f8ba064f571618ed5a7fb8caeaec59186d3f3bd1
tanuja-sarode <63042731+tanuja-sarode@users.noreply.github.com>
2020-05-29 11:05:35 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -36,7 +36,3 @@ cacheSolve <- function(x, ...) {
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
